a806c5f2a67d6206d15ef12e579530e2f75b473f
Ahmed Khaled <ahmedkhaled11119999@gmail.com>
2021-06-04 18:29:17 +0200

    - Added user_id to reviews serializer

--- treat_min/user_reviews/api/serializers.py
+++ treat_min/user_reviews/api/serializers.py
@@ -4,13 +4,17 @@ from ..models import ClinicReview
 
 class ReviewSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField('get_name')
+    user_id = serializers.SerializerMethodField('get_user_id')
 
     def get_name(self, obj):
         return obj.user.user.name
 
+    def get_user_id(self, obj):
+        return obj.user.id
+
     class Meta:
         model = ClinicReview
-        fields = ['name', 'date', 'rating', 'review']
+        fields = ['user_id', 'name', 'date', 'rating', 'review']
 
 
 class RateSerializer(serializers.ModelSerializer):
