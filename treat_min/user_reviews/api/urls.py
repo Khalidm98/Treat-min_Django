d5d17fde359222f6108731bf4f495e85052e5dd7
Khalidm98 <khalid.refaat98@gmail.com>
2021-08-23 16:02:26 +0200

    Updated rate and review API

--- treat_min/user_reviews/api/urls.py
+++ treat_min/user_reviews/api/urls.py
@@ -3,6 +3,5 @@ from .views import ReviewAPI, RateAPI
 
 urlpatterns = [
     path('<str:entities>/<int:entity_id>/details/<int:detail_id>/reviews/', ReviewAPI.as_view()),
-    path('<str:entities>/<int:entity_id>/details/<int:detail_id>/rate/', RateAPI.as_view()),
-    # change this url
+    path('user/appointments/<str:entities>/<int:appointment_id>/review/', RateAPI.as_view()),
 ]
--- treat_min/user_reviews/api/views.py
+++ treat_min/user_reviews/api/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,6 +7,7 @@ from knox.auth import TokenAuthentication
 
 from ...accounts.api.views import get_user
 from ...entities_details.api.views import check_detail
+from ...user_appointments.models import ClinicAppointment, ServiceAppointment
 from ..models import ClinicReview, ServiceReview
 from .serializers import ReviewSerializer, RateSerializer
 
@@ -28,29 +30,53 @@ class RateAPI(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, entities, entity_id, detail_id):
-        result = check_detail(entities, entity_id, detail_id)
-        if isinstance(result, Response):
-            return result
+    def post(self, request, entities, appointment_id):
+        try:
+            if entities == 'clinics':
+                appointment = ClinicAppointment.objects.get(id=appointment_id)
+                detail = appointment.schedule.clinic
+            elif entities == 'services':
+                appointment = ServiceAppointment.objects.get(id=appointment_id)
+                detail = appointment.schedule.service
+            else:
+                return Response({"details": "Page not found!"}, status.HTTP_404_NOT_FOUND)
+
+        except (ClinicAppointment.DoesNotExist, ServiceAppointment.DoesNotExist):
+            return Response(
+                {"details": "{0} appointment not found!".format(entities[0:len(entities) - 1])},
+                status.HTTP_404_NOT_FOUND
+            )
+
+        user = get_user(request)
+        if appointment.user != user:
+            return Response(
+                {"details": "This appointment doesn't belong to this user!"},
+                status.HTTP_400_BAD_REQUEST
+            )
+
+        if appointment.appointment_date > date.today():
+            return Response(
+                {"details": "You cannot review a future appointment!"},
+                status.HTTP_400_BAD_REQUEST
+            )
 
         serializer = RateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         rating = request.data.get('rating')
-        user = get_user(request)
 
         try:
-            review = result.reviews.get(user=user)
-            result.rating_total = result.rating_total - int(review.rating) + int(rating)
-            result.save()
+            review = detail.reviews.get(user=user)
+            detail.rating_total = detail.rating_total - int(review.rating) + int(rating)
+            detail.save()
             review.review = request.data.get('review')
             review.rating = rating
             review.save()
             return Response({"details": "Your review was updated successfully."})
 
         except (ClinicReview.DoesNotExist, ServiceReview.DoesNotExist):
-            result.rating_total = result.rating_total + int(rating)
-            result.rating_users = result.rating_users + 1
-            result.save()
+            detail.rating_total = detail.rating_total + int(rating)
+            detail.rating_users = detail.rating_users + 1
+            detail.save()
 
             params = {
                 'user': user,
@@ -58,9 +84,9 @@ class RateAPI(APIView):
                 'review': request.data.get('review')
             }
             if entities == 'clinics':
-                ClinicReview.objects.create(clinic_id=detail_id, **params)
+                ClinicReview.objects.create(clinic_id=detail.id, **params)
             elif entities == 'services':
-                ServiceReview.objects.create(service_id=detail_id, **params)
+                ServiceReview.objects.create(service_id=detail.id, **params)
 
             return Response(
                 {"details": "Your review was saved successfully."},
